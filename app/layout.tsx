9061d7f46e2ced4902cd8d87c132f86509df140b
Rez <82224862+Rezthecoder@users.noreply.github.com>
2025-07-07 09:50:16 +0900

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,6 +14,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        {/* Google AdSense */}
+        <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4461751532021246"
+          crossOrigin="anonymous"
+        ></script>
+      </head>
       <body>{children}</body>
     </html>
   )
